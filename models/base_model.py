8aaa49a89a01833be89d89de652930531af173c5
etimeminent@gmail.com <etimeminent@gmail.com>
2023-12-10 06:55:13 -0800

    updating

--- models/base_model.py
+++ models/base_model.py
@@ -9,13 +9,33 @@ from datetime import datetime
 
 class BaseModel:
     """
+    Attributes:
+    id (str): id of an instance of this class
+    created_at (datetime): time an imstance was created
+    updated_at (datetime): time an instance of this object is updated
     Defining a base class
     """
-    def __init__(self):
-        """ Creating a public instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """ Creating a public instance attributes
+         Initalizes an object of class Basemodel
+        Args:
+            args (tuple): variable lenght argument
+            kwargs (dict): contains instance attributes and values
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.created_at = datetime.fromisoformat(value)
+                elif key == "updated_at":
+                    self.updated_at = datetime.fromisoformat(value)
+                elif key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Implementing a method to print string"""
